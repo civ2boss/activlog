c4e2d61ef7f61a9c8478de12e750b909ba28d326
Robin Ting <rting@h133.s112.dyn.qc.cuny.edu>
2010-03-17 10:54:30 -0400

    Added authlogic gem and created a user model

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100311062114) do
+ActiveRecord::Schema.define(:version => 20100317145233) do
 
   create_table "logs", :force => true do |t|
     t.string   "name"
     t.string   "contact_type"
     t.text     "description"
     t.integer  "social_security"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
