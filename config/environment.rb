07fba8a7abbf4f6f98b0b3cc0c4b934d06ea48f2
Robin Ting <civ2boss@gmail.com>
2011-12-04 00:51:20 -0500

    Updating depreciated or changed code for rails 2.3.14

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "authlogic"
+  config.gem "authlogic", :version => '2.1.6'
   config.gem "cancan"
   config.gem "fastercsv"
   config.gem "crafterm-comma", :lib => "comma", :source => "http://gems.github.com"
